6b97780fe11aab0f4fdd1ae758ca787805be1d87
John Armstrong <j.armstrong484@gmail.com>
2016-11-05 14:07:58 +1300

    Added smartbulb component as the main component of the application. It should contain all other components

---
+++ components/smartbulb.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Hello from './hello.jsx';
+
+export default class SmartBulb extends React.Component {
+    render() {
+        return (
+            <Hello name="World"/>
+        );
+    }
+}
