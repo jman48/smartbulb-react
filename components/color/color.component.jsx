780d98d7a82bd09a123b70301f275990cbf72c80
John Armstrong <j.armstrong484@gmail.com>
2016-11-08 19:03:50 +1300

    Will now update the color when slider value changes

--- components/color/color.component.jsx
+++ components/color/color.component.jsx
@@ -5,17 +5,33 @@ import ColorPicker from '../colorPicker/colorPicker.component.jsx';
 import Style from './_color.style.scss';
 
 /**
- * This is the main component of our application.
+ * This component encapsulates all the color picker stuff
  */
 export default class Color extends React.Component {
+
+	constructor() {
+		super();
+		this.updateColor = this._updateColor.bind(this);
+
+		this.colors = {
+			red: 0,
+			green: 0,
+			blue: 0
+		}
+	}
+
 
 	render() {
 		return (
 			<div className="color">
-				<ColorPicker name="red"/>
-				<ColorPicker name="green"/>
-				<ColorPicker name="blue"/>
+				<ColorPicker name="red" color={this.colors.red} onSelected={this.updateColor}/>
+				<ColorPicker name="green" color={this.colors.green} onSelected={this.updateColor}/>
+				<ColorPicker name="blue" color={this.colors.blue} onSelected={this.updateColor}/>
 			</div>
 		);
+	}
+
+	_updateColor(color, value) {
+		this.colors[color] = value;
 	}
 }
--- components/colorPicker/colorPicker.component.jsx
+++ components/colorPicker/colorPicker.component.jsx
@@ -11,14 +11,20 @@ export default class ColorPicker extends React.Component {
 	constructor(props) {
 		super(props);
 		this.props = props;
+
+		this.updateColor = this._updateColor.bind(this);
 	}
 
 	render() {
 		return (
 			<div className={this.props.name}>
-				<Slider defaultValue={0} max={255}/>
+				<Slider step={1} defaultValue={this.props.color} max={255} onChange={this.updateColor}/>
 				<span className="picker-text">{this.props.name}</span>
 			</div>
 		);
+	}
+
+	_updateColor(event, value) {
+		this.props.onSelected(this.props.name, value);
 	}
 }
