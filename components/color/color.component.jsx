958dcf5cddde3f878633b92225a44704a1d4587b
John Armstrong <j.armstrong484@gmail.com>
2016-11-12 11:21:46 +1300

    Changed to only update the color on drag stop. This helps stopping the server getting spamed with to many requests

--- components/color/color.component.jsx
+++ components/color/color.component.jsx
@@ -22,7 +22,6 @@ export default class Color extends React.Component {
 		}
 	}
 
-
 	render() {
 		return (
 			<div className="color">
--- components/colorPicker/colorPicker.component.jsx
+++ components/colorPicker/colorPicker.component.jsx
@@ -4,27 +4,39 @@ import Slider from 'material-ui/Slider';
 import Style from './_colorPicker.style.scss';
 
 /**
- * This is the main component of our application.
+ * This component lets a user select a color
  */
 export default class ColorPicker extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.props = props;
+		this.colorValue = this.props.color;
 
 		this.updateColor = this._updateColor.bind(this);
+		this.updateValue = this._updateValue.bind(this);
 	}
 
 	render() {
 		return (
 			<div className={this.props.name}>
-				<Slider step={1} defaultValue={this.props.color} max={255} onChange={this.updateColor}/>
+				<Slider step={1} defaultValue={this.colorValue} max={255} onDragStop={this.updateColor} onChange={this.updateValue}/>
 				<span className="picker-text">{this.props.name}</span>
 			</div>
 		);
 	}
 
-	_updateColor(event, value) {
-		this.props.onSelected(this.props.name, value);
+	/**
+	 * Update the color value on parent. Only do this on deselect so that we do not spam server with requests
+	 */
+	_updateColor() {
+		this.props.onSelected(this.props.name, this.colorValue);
+	}
+
+	/**
+	 * Update the color value locally
+	 */
+	_updateValue(event, newColorValue) {
+		this.colorValue = newColorValue;
 	}
 }
