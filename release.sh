fb4f6484360d3ff1e19bac55c64a00713b727082
John Armstrong <j.armstrong484@gmail.com>
2016-11-12 12:00:04 +1300

    Release script will now package up application

--- release.sh
+++ release.sh
@@ -8,6 +8,8 @@ function title {
 	printf "\n\n**** $1 ****\n\n"
 }
 
+RELEASE_DIR=release
+
 title "Patching version"
 npm -no-git-tag-version version patch
 VERSION=$(node -p -e "require('./package.json').version")
@@ -20,12 +22,16 @@ git push --tags
 
 title "Clearing old build files and folders"
 rm bundle.js
-rm -rf dist
+rm -rf $RELEASE_DIR
 
 title "Creating release $VERSION"
-mkdir dist
+mkdir $RELEASE_DIR
 npm run build
-cp index.html dist
-cp bundle.js dist
+cp index.html $RELEASE_DIR
+cp bundle.js $RELEASE_DIR
+cp package.json $RELEASE_DIR
 
-title "Release done. Please check the dist folder for release"
+title "Packaging application"
+RELEASE_FILE=$(npm pack $RELEASE_DIR)
+
+title "Release done. File $RELEASE_FILE created for release"
